70dcb1f2b784b047aab0d8163de120a7f4454aed
nirmal.manandhar <nirmal.manandhar@medicaldirector.com>
2023-04-12 08:00:12 +1000

    Added stop script

--- start_acs.sh
+++ start_acs.sh
@@ -4,5 +4,5 @@
 JAR_FILE=/var/www/tomcat-8.5.54/springbootwar/acs.war
 
 # Run the Spring Boot application and store the PID in a file
-java -jar $JAR_FILE >/dev/null 2>&1 &
-echo $! > springboot.pid
+java -jar $JAR_FILE >/var/www/tomcat-8.5.54/springbootwar/acs.log 2>&1 &
+nohup java -jar /path/to/app/hello-world.jar > /path/to/log.txt 2>&1 &
---
+++ stop_acs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+kill $(cat /var/www/tomcat-8.5.54/springbootwar/)
